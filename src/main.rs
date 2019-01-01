47447ab083bf6ba2b4a6fcdc6df54695da26e1d8
Gordon Stewart <gstew5@gmail.com>
2019-01-01 18:17:42 -0500

    QRE deriv with a simple benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::iter::Iterator;
 use std::clone::Clone;
 use std::sync::Arc;
+use std::time::{Instant};
 
 #[derive(Clone)]
 enum QRE<D> {
@@ -105,5 +106,58 @@ fn deriv<D>(q: QRE<D>, d: &D) -> Vec<QRE<D>> where D: Clone {
     }
 }
 
+struct Solve<D> {
+    pub state: Vec<QRE<D>>,
+}
+
+impl <D> Solve<D> where D: Clone {
+    pub fn new(q: QRE<D>) -> Self {
+        Self {state: vec![q]}
+    }
+
+    pub fn update(&mut self, d: D) -> () {
+        let mut vnew = Vec::new();
+        for q in &self.state[..] {
+            vnew.append(&mut deriv(q.clone(), &d))
+        };
+        self.state = vnew
+    }
+
+    pub fn output(&self) -> Result<f64, String> {
+        let mut cnew = Vec::new();
+        for q in &self.state[..] {
+            cnew.append(&mut epsilon(&q.clone()))
+        };
+        if cnew.len() == 1 { Ok(cnew[0]) }
+        else {
+            Err("undefined".to_string())
+        }
+    }
+}
+
+fn true_f64(x: &f64) -> bool { true }
+fn id_f64(x: &f64) -> f64 { *x }
+fn sum_f64(x: f64, y: f64) -> f64 { x + y }
+
 fn main() {
+    let f = Sat{phi: true_f64, op: id_f64};
+    let r = Iter{init: Box::new(f.clone()),
+                 body: Box::new(f),
+                 op: sum_f64};
+    let mut s = Solve::new(r);
+
+    //Compute T(1000) using QREs
+    let now = Instant::now();
+    for x in 0..1001 { s.update(x as f64) }
+    println!("{:?}", s.output());
+    let elapsed = now.elapsed();
+    println!("QRE time = {}s, {}ms", elapsed.as_secs(), elapsed.subsec_millis());
+
+    //Compute T(1000) by iteration
+    let mut t = 0.0;
+    let now2 = Instant::now();    
+    for x in 0..1001 { t = t + (x as f64) }
+    println!("{:?}", t);
+    let elapsed2 = now.elapsed();
+    println!("time = {}s, {}ms", elapsed2.as_secs(), elapsed2.subsec_millis());
 }
